760bc9b83e19b4bde1a0b5f5a940a62abb6fb859
DHANIKUSUMA <dhaniksmpr@gmail.com>
2024-09-21 15:15:19 +0700

    latihan coding 21 september 2024 bismillah calon programar handal diluar handal sehandal handalnya

---
+++ main.py
@@ -0,0 +1,9 @@
+nilai =int(input("masukkan nilai: "))
+
+if (nilai > 90):
+    print("selamat anda lulus ")
+elif (nilai > 90 and nilai <100) :
+    print ("selama anda lulus, dengan nilai memuaskan")
+else :
+    print("anda masih belum lulus tes, semangat dan coba lagi")
+ 
